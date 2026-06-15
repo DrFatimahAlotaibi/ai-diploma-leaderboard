3cbf7058dfb98c0f201d4cc259cd5c1c075ae2fa
DrFatimahAlotaibi <f.dhaifallah.te@tuwaiq.edu.sa>
2026-06-15 20:14:51 +0300

    Update data.js

--- data.js
+++ data.js
@@ -9,42 +9,42 @@ const diplomaData = {
       id: 1,
       name: "The Innovators",
       members: ["Mohammed Almalke", "Abdulraham Almuhareb", "Nouf Alenzi", "Afnan Alshalahi"],
-      scores: { quiz: 7, lab: 10, explanation: 0 },
+      scores: { quiz: 7, lab: 20, explanation: 0 },
       badges: ["Clean Code Badge", "3-Week Quiz Streak"]
     },
     {
       id: 2,
       name: "The Legends",
       members: ["Yasser Altayari", "Yahya Majrashi", "Waim Alatawi", "Waad Alatawi"],
-      scores: { quiz: 9, lab: 10, explanation: 0 },
+      scores: { quiz: 9, lab:20, explanation: 0 },
       badges: ["Best Explanation", "Resource Helper"]
     },
     {
       id: 3,
       name: "LAMA",
       members: ["Lujain Alrushoud", "Atheer Almajnoni", "Mohammed Albalawi", "Abdulrahman Alqurashi"],
-      scores: { quiz: 10, lab: 10, explanation: 0 },
+      scores: { quiz: 10, lab: 20, explanation: 0 },
       badges: ["Rising Team"]
     },
     {
       id: 4,
       name: "The Geeks",
       members: ["Retaj Albogami", "Yazeed Alhifzi", "Ghady Aldabash", "Omar Alanazi"],
-      scores: { quiz: 8, lab: 10, explanation: 1 },
+      scores: { quiz: 8, lab: 20, explanation: 1 },
       badges: ["Mini Lab Star", "Debug Hero"]
     },
     {
       id: 5,
       name: "Engs",
       members: ["Osama Alhasson", "Jawaher Alyami", "Hussam Almozaini", "Layan Alshedokhi"],
-      scores: { quiz: 10, lab: 10, explanation: 0 },
+      scores: { quiz: 10, lab: 20, explanation: 0 },
       badges: ["Creative Thinkers"]
     },
     {
       id: 6,
       name: "The mean team",
       members: ["Leen Alaslani", "Nawal Alkhilifah", "Salman Alsheheri", "Saleh Almahmood"],
-      scores: { quiz: 10, lab: 10, explanation: 0 },
+      scores: { quiz: 10, lab: 20, explanation: 0 },
       badges: ["Teamwork Badge"]
     }
   ],
