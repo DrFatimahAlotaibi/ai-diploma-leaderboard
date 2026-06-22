8f7986c7b3d52d04b34a036e8e1da1a4c43118c7
DrFatimahAlotaibi <f.dhaifallah.te@tuwaiq.edu.sa>
2026-06-22 20:46:51 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const diplomaData = {
-  currentWeek: "Week 3",
+  currentWeek: "Week 4",
   courseTitle: "Mathematics, Linear Algebra and Probability for AI and Machine Learning",
   year: "2026",
   instructors: "Dr. Fatimah Alotaibi and Ms. Banan Alnemeri",
